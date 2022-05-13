864429d28ad8aad8bcb362bd71f8e64cd673c578
joysonjson <joysonpathra@gmail.com>
2022-05-13 23:12:47 +0530

    changing to sunny

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,17 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">SUNNY </span>
+            from <span className="purple"> Nelloe, AP.</span>
+            <br />I am studying BSC in Electronics
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
+          <img
+            src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2016/02/Headshot-Photography-London-0997.jpg?ssl=1"
+            height="100"
+          />
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
